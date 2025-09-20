77a5310a8ce682291d2655f2e37523ebd927b37d
Daniel Andrés <Daniel.Andres.dev@Gmail.com>
2025-09-20 11:26:06 -0500

    Initial 🎮

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,5 +1,49 @@
 extends CharacterBody3D
 
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 4.5
+@export var SENSITIVITY_GAZE = 0.025
 
-func _physics_process(delta: float) -> void:
-	pass
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * 0.005)
+		camera.rotate_x(-event.relative.y * 0.005)
+		camera.rotation.x = clamp(camera.rotation.x, -1.2, 1.2)
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	var input_vision = Input.get_vector("ui_right_2", "ui_left_2", "ui_up_2", "ui_down_2")
+	if input_vision:
+		head.rotation.y += input_vision.x * SENSITIVITY_GAZE
+		camera.rotation.x += input_vision.y * SENSITIVITY_GAZE
+		camera.rotation.x = clamp(camera.rotation.x, -1.2, 1.2)
+		
+	move_and_slide()
