5c7b7478b1c99117504b38a0c32a36d7e1712c4c
Daniel Andrés <Daniel.Andres.dev@Gmail.com>
2025-09-20 15:10:13 -0500

    <.*.>

--- ui/menu.gd
+++ ui/menu.gd
@@ -1,10 +1,26 @@
 extends Control
 
 @onready var start := %Start
+@onready var credited := %Credited
+@onready var exit := %Exit
+@onready var display_menu := %DisplayMenu
+
+
 var option_focus := 0
 
 func _ready() -> void:
-	start.connect("button_down", _start)
-
-func _start() -> void:
-	get_tree().change_scene_to_file('res://levels/test/scene.tscn')
+	start.connect("button_down", func() -> void:
+		get_tree().change_scene_to_file('res://levels/test/scene.tscn')
+	)
+	credited.connect("button_down", func() -> void:
+		clear_display()
+		display_menu.add_child(load("res://ui/credited.tscn").instantiate())
+	)
+	exit.connect("button_down", func() -> void:
+		get_tree().quit()
+	)
+	
+	
+func clear_display() -> void:
+	for x in display_menu.get_children():
+		(x as Node).queue_free()
--- ui/pause.gd
+++ ui/pause.gd
@@ -1,8 +1,29 @@
 extends Control
 
+
 @onready var resumen := %Resumen
+@onready var credited := %Credited
+@onready var display_pause := %DisplayPause
+@onready var reset := %Reset
+
 
 func _ready() -> void:
+	LEVELS.listen_paused.connect(func(value: bool) -> void:
+		if value:
+			clear_display()
+	)
 	resumen.connect("button_down", func () -> void:
 		LEVELS.toogle_pause(false)
 	)
+	credited.connect("button_down", func() -> void:
+		clear_display()
+		display_pause.add_child(load("res://ui/credited.tscn").instantiate())
+	)
+	reset.connect("button_down", func () -> void:
+		get_tree().change_scene_to_file('res://ui/menu.tscn')
+	)
+
+
+func clear_display() -> void:
+	for x in display_pause.get_children():
+		(x as Node).queue_free()
